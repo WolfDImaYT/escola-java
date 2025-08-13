e2f743a785261c7f51496cd4b1de35084fb7cfec
WolfDimaYT <migueljipe15@gmail.com>
2025-08-13 17:20:26 -0300

    sla

--- src/main/java/com/etec/escola/Controller/AlunoController.java
+++ src/main/java/com/etec/escola/Controller/AlunoController.java
@@ -1,4 +1,12 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Service.AlunoService;
+
 public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
 }
